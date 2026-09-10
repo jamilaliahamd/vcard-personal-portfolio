38cad8662908c2a17b076b3f2a3f97b88dad600c
Portfolio Publisher <publisher@example.com>
2026-09-11 01:04:12 +0500

    Connect live Firebase workspace database

--- sync-config.js
+++ sync-config.js
@@ -1,8 +1,8 @@
 // Add the Firebase web app settings here before deployment.
 window.VOLTWISE_FIREBASE_CONFIG = {
-  apiKey: '',
+  apiKey: 'AIzaSyBqdnEjzyOz6S3qdwK4CGIBWMxXAwBYrTk',
   authDomain: 'arkan-al-omda-limited-co.firebaseapp.com',
   databaseURL: 'https://arkan-al-omda-limited-co-default-rtdb.asia-southeast1.firebasedatabase.app',
   projectId: 'arkan-al-omda-limited-co',
-  appId: ''
+  appId: '1:202976573358:web:b4e4d83a18c1c41f593a3f'
 };
