51b43ed68acb9f528252db7fea0853b9df3a71c8
Portfolio Publisher <publisher@example.com>
2026-09-11 00:45:01 +0500

    Configure Firebase database endpoint

--- sync-config.js
+++ sync-config.js
@@ -1,8 +1,8 @@
 // Add the Firebase web app settings here before deployment.
 window.VOLTWISE_FIREBASE_CONFIG = {
   apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
+  authDomain: 'arkan-al-omda-limited-co.firebaseapp.com',
+  databaseURL: 'https://arkan-al-omda-limited-co-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'arkan-al-omda-limited-co',
   appId: ''
 };
